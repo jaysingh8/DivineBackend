d70a8588c78ff9a3f0d8b0b7383e6666f2f169a0
Jay Singh <jaysinghrajput265@gmail.com>
2026-07-21 11:00:56 +0530

    first commit

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,12 +5,12 @@ import jwt from "jsonwebtoken"
 async function sendToken(user , res, message) {
     const token = jwt.sign({id:user._id},config.JWT_SECRET,{expiresIn:"7d"})
 
-    res.cookie("token", token/*, {
+    res.cookie("token", token, {
         httpOnly: true,
         secure: true,
         sameSite: 'none',
         maxAge: 7 * 24 * 60 * 60 * 1000
-    }*/)
+    })
 
     res.status(200).json({
         message,
